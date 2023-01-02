8096380857945a4dd080447d1abd27c709f0ada9
euys <eunseo2867@naver.com>
2023-01-02 23:49:09 +0900

    INIT : JDBC Day00 JDBC Coding
    1. JDBC Coding 절차 구현해보기

--- src/com/my/jdbc/day00/JDBCRun.java
+++ src/com/my/jdbc/day00/JDBCRun.java
@@ -14,33 +14,35 @@ public class JDBCRun {
 		 * 2. DBMS 연결 생성	(연결된 상태)
 		 * 3. Statement 객체 생성	(쿼리문 실행준비)
 		 * 4. SQL 전송			(쿼리문 실행된 상태)
-		 * 5. 결과값 받기	(ResultSet을 받은 상태) -> 후처리
-		 * 6. 자원해제		(close())
+		 * 5. 결과값 받기	(ResultSet을 받은 상태) -> 후처리 필요
+		 * 6. 자원해제		(close();)
 		 * 
 		 * */
 		
 		try {
-			// Referenced Libraries에 있는 ojdbc6.jar 파일 사용하기 위해 쓴 경로
+			// Referenced Libraries에 있는 ojdbc6.jar파일(실제 드라이버) 사용하기 위해 작성한 경로
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			String user = "KH";	// KH라는 계정
-			String password = "KH";	// 비번: KH -> conn에 들어가 있는 상태
+			String user = "KH";	// KH라는 계정(사용하고 있는 DB계정 작성)
+			String password = "KH";	// 비밀번호 KH -> conn에 들어가 있는 상태
 			String sql = "SELECT * FROM EMPLOYEE";
 			
 			// 1. 드라이버 등록
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
-			// 2. DB 연결 생성 -> 인터페이스(connection) 생성 및
-			// 클래스 바로 적고 메소드 호출 가능한 이유? DriverManager에 있는 getConection메소드가 Static메소드였기 때문에.
+			// 2. DB 연결 생성 -> 인터페이스(Connection) 생성 및 생성된 DB 저장
+			// Q. 클래스 내에서 객체 생성을 하지 않고도 메소드 호출 가능한 이유? 
+			// A. DriverManager에 있는 getConection메소드가 Static메소드였기 때문이다
 			Connection conn = DriverManager.getConnection(url, user, password);
 			
 			// 3. 쿼리문 실행준비(Statement 객체 생성)
 			Statement stmt = conn.createStatement();
 			
-			// 4. 쿼리문 실행 (RESULTSET을 실행, 쿼리문 실행하기 위해 executeQuery메소드)
+			// 4. 쿼리문 실행 (쿼리문 실행 후 ResultSet 인터페이스인 rset에 DB저장
+			// 				  (*쿼리문 실행하기 위해 'executeQuery메소드' 사용*)
 			ResultSet rset = stmt.executeQuery(sql);
 			
-			// 후처리 - DB에서 가져온 데이터를 사용
-			// (while문으로 변경해보기)
+			// 5. 후처리 - DB에서 가져온 데이터를 사용
+			// (while문으로 변경해보기 > DB안에 있는 23개의 데이터 출력)
 			while(rset.next()) {
 				System.out.println("사원 아이디 : " + rset.getNString("EMP_ID"));
 			/*if(rset.next()) {
@@ -48,7 +50,7 @@ public class JDBCRun {
 				*/
 			}
 			
-			// 자원해제
+			// 6. 자원해제
 			rset.close();
 			stmt.close();
 			conn.close();
